528723c5a4ea97b1f97b993d60549d16d57ad003
Davey Agrinsoni <dgagrinsoni@gmail.com>
2021-10-13 14:10:23 -0400

    Added the Basic Java folder which will contain basic java practice programs

---
+++ Basic-Java/practice.java
@@ -0,0 +1,23 @@
+public class practice{
+	public static void main(String[] args){
+	int alp = 65;
+	for(int i=0; i < 26; i++){
+		System.out.println((char)(alp+i));
+		}
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
