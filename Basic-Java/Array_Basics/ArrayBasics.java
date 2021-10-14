efc44b3a86f0af0118258eb155fb9d1878e9c272
Davey Agrinsoni <dgagrinsoni@gmail.com>
2021-10-14 17:01:15 -0400

    Got rid of a few outdated comments to help clean up and clarify the code.

--- Basic-Java/Array_Basics/ArrayBasics.java
+++ Basic-Java/Array_Basics/ArrayBasics.java
@@ -1,7 +1,7 @@
 public class ArrayBasics{
     
     public static String[] reverse(String[] input){
-        // Print all elements of names array backwards
+        // Reverses all the elements of the inputed array and returns a new reversed array.
         String[] rev = new String[input.length];
         int j = 0;
         for(int i = input.length - 1; j < input.length; i--){
@@ -12,19 +12,11 @@ public class ArrayBasics{
     }
 
     public static void main(String[] args){
-        System.out.println("Hello World!");
-
+        //System.out.println("Hello World!");
         String[] revarg = reverse(args);
-        /*for(String l : rev){
-			System.out.print(l);
-        }*/
-        for(int i = 0; i < revarg.length; i++){
-             System.out.print(revarg[i]);
-            }
-        //String[] car = {"R", "a", "c", "e","c","a","R."};
-        //String[] batch = new String[3]; 
         
+        for(int i = 0; i < revarg.length; i++){
+             System.out.print(revarg[i]+" ");
+        }
     }
-
-
 }
