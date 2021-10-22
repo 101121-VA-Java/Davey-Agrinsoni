5a468423811d2a3675612cea25ba834cd98fe3ab
Davey Agrinsoni <dgagrinsoni@gmail.com>
2021-10-22 18:07:43 -0400

    Exception for the calculator if there is an ArithmeticException

--- Week_2/Basic_Java/maven-demo/src/main/java/com/revature/exceptions/CalculatorException.java
+++ Week_2/Basic_Java/maven-demo/src/main/java/com/revature/exceptions/CalculatorException.java
@@ -1,10 +1,9 @@
 package com.revature.exceptions;
 
 public class CalculatorException extends ArithmeticException{
-
-	/**
-	 * 
-	 */
+		/**
+		 * 
+		 */
 	private static final long serialVersionUID = 1L;
 
 }
