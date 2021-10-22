2dacc9c0ff8f91c518074996d4e7c29a96ace734
Davey Agrinsoni <dgagrinsoni@gmail.com>
2021-10-22 17:33:03 -0400

    Setting up Maven demo

---
+++ Week_2/Basic_Java/maven-demo/src/main/java/com/revature/Calculator.java
@@ -0,0 +1,35 @@
+package com.revature;
+
+public class Calculator {
+
+	/*
+	 * Should be able to
+	 * 	- add
+	 * 	- subtract
+	 * 	- multiply
+	 * 	- divide
+	 * 		- throw Calculator Exception (Runtime) when attempting to divide by 0
+	 *	- isPrime: checks if a number is Prime
+	 */
+	
+	public double add(double x, double y) {
+		return x + y;
+	}
+	
+	public double subtract(double x, double y) {
+		return x - y;
+	}
+	
+	public double multiply(double x, double y) {
+		return x * y;
+	}
+	
+	public double divide(double x, double y) {
+		return x / y;
+	}
+	
+	public boolean isPrime(int i) {
+		//Logic
+		return true;
+	}
+}
