aa01d2fd8e2a4ff8b8562c131d39b406da3043a3
Davey Agrinsoni <dgagrinsoni@gmail.com>
2021-10-28 17:48:59 -0400

    Adding any changed files from week 2

--- Week_2/Basic_Java/custom-oop/src/main/java/com/revature/models/User.java
+++ Week_2/Basic_Java/custom-oop/src/main/java/com/revature/models/User.java
@@ -1,5 +1,78 @@
 package com.revature.models;
 
-public class Test {
+public class User {
+
+	private int age;
+	private String name;
+	private Task task;
+
+	// static variable
+	public static int numberOfUsers;
+
+	public User() {
+		super();
+	}
+
+	public User(int age) {
+		super();
+		this.age = age;
+	}
+
+	public User(int age,String name) {
+		super();
+		this.age = age;
+		this.name = name;
+	}
+
+	// method that return the age of that instance
+	public int getAge() {
+		return age;
+	}
+
+
+	public String getUsername() {
+		return name;
+	}
+
+	public void setUsername(String name) {
+		this.name = name;
+	}
+	
+	// method that changes the age of that instance
+	public void setAge(int age) {
+		if (age < 0) {
+			this.age = 0;
+		} else {
+			this.age = age;
+		}
+	}
+
+	public static int getNumberOfUsers() {
+		return numberOfUsers;
+	}
+
+	public static void setNumberOfUsers(int numberOfUsers) {
+		User.numberOfUsers = numberOfUsers;
+	}
+
+	public static int getNumOfUsers() {
+		return numberOfUsers;
+	}
+
+
+	public User(String username, String password, int age, Task task) {
+		super();
+		this.name = username;
+		this.age = age;
+		this.task = task;
+	}
+
+	public Task getTask() {
+		return task;
+	}
+
+	public void setTask(Task task) {
+		this.task = task;
+	}
 
 }
--- Week_2/Basic_Java/custom-oop/src/main/java/com/revature/services/OopService.java
+++ Week_2/Basic_Java/custom-oop/src/main/java/com/revature/services/OopService.java
@@ -1,5 +1,91 @@
 package com.revature.services;
 
-public class Stuff {
+import com.revature.models.User;
 
+public class OopService {
+
+	// examples of access modifiers
+	private int i;
+	protected String s;
+	String something; // default access modifier
+
+	public void doEncapsulation() {
+		User u = new User();
+		User k = new User();
+		u.setUsername("Davey");
+		u.setAge(-25);
+
+		System.out.println("u age: " + u.getAge());
+		System.out.println(u);
+		
+	}
+
+	public void doInheritance() {
+		User u = new User();
+
+		System.out.println(u);
+
+		ImportantTask it = new ImportantTask("Quiz", LocalDate.now());
+
+		it.stress();
+
+		System.out.println(it);
+	}
+
+	public void doPolymorphism() {
+		// method overriding
+//		Task t1 = new Task();
+//
+//		System.out.println("This should print toString() from Task class: " + t1);
+
+		ImportantTask it = new ImportantTask();
+
+		System.out.println("This should print toString() from Task class: " + it);
+
+		BoringTask bt = new BoringTask();
+
+		System.out.println("This should print toString() from BoringTask class: " + bt);
+
+		// method overloading
+
+		bt.setDueDate(LocalDate.now());
+
+		System.out.println("Due date before all: " + bt.getDueDate());
+
+		bt.procrastinate();
+
+		System.out.println("Due date after procrastinate(): " + bt.getDueDate());
+
+		bt.procrastinate(5);
+
+		System.out.println("Due date after procrastinate(5): " + bt.getDueDate());
+
+		bt.procrastinate(5, 2);
+
+		System.out.println("Due date after procrastinate(5, 2): " + bt.getDueDate());
+
+		// Upcasting
+		Task t = new BoringTask();
+		Task t2 = new ImportantTask();
+
+		Task[] tasks = { t, t2 };
+
+		for (Task task : tasks) {
+			System.out.println(task.toString());
+		}
+		
+//		BoringTask boring = (BoringTask) new Task();
+		
+
+	}
+	
+	public void doAbstraction() {
+		Task t; // can declare a variable of type task (for upcasting) 
+//		t = new Task(); // cannot instantiate because the class is abstract
+		t = new BoringTask();
+		
+		t.doTask(); // boring task had to override the abstract doTask method declared in the Task class
+		Doable doa = new BoringTask();
+		doa.doSomethingElse();
+	}
 }
