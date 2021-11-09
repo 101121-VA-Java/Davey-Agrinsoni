23082bfbe97e736b94c337790c716df8abae73b4
Davey Agrinsoni <dgagrinsoni@gmail.com>
2021-11-09 16:54:24 -0500

    Uploading frame of InventoryController

--- Project_0/Prototype2/src/main/java/com/revature/controllers/InventoryController.java
+++ Project_0/Prototype2/src/main/java/com/revature/controllers/InventoryController.java
@@ -1,5 +1,28 @@
 package com.revature.controllers;
 
-public class InventoryController {
+import java.util.Scanner;
 
+import com.revature.repositories.AlbumPostgres;
+import com.revature.repositories.BagPostgres;
+
+public class InventoryController {
+	private static AlbumPostgres ap = new AlbumPostgres();
+	private static BagPostgres bagp = new BagPostgres();
+	Scanner sc = new Scanner(System.in);
+
+	public void addToShop() {
+		//TODO
+	}
+	
+	public void removeFromShop() {
+		//TODO
+	}
+	
+	public void manageOrders() {
+		//TODO
+	}
+	
+	public void editPrice() {
+		//TODO
+	}
 }
